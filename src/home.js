df20be5186636c07b1c618cbd515afe6ee648b55
PatriciaMuench <pmuench89@gmail.com>
2019-08-15 17:19:46 -0400

    Mostly cleaned up home.js, *including removed some attempts from styling/logo that I COULD want to look back at.

--- src/home.js
+++ src/home.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Link } from "react-router-dom";
-// import Bands from './bands';
-// import Venues from './venues';
 import logo from './android-chrome-512x512-copy.png';
 import './App.css';
 
@@ -10,20 +8,12 @@ class Home extends Component {
     return (
       // note: I don't know if forceRefresh is desirable here, but I also don't know why things weren't rerendering on link clicks
       <Router forceRefresh>
-      {/* <img src={logo} alt="logo" style={{'margin': '100px 0px 0px -500px', 'padding': '-100px'}}/> */}
-      {/* , 'position': 'relative', 'left': '100px' */}
         <div className="App-header">
           <h1 className="main"><img src={logo} alt="logo" className="logo" height="100" width="100" />Who's Playin'</h1>
-        {/* </div> */}
-          {/* <div className="App-body">  */}
           <p className="main">
             Find live music events organized by venue, band, or location
           </p>
-          {/* <div className="App-body"> */}
-            <span><Link to="/bands">Bands</Link> &nbsp; <Link to="/venues">Venues</Link></span>
-
-            {/* <Route path="/bands" component={Bands}/> */}
-          {/* </div> */}
+          <span><Link to="/bands">Bands</Link> &nbsp; <Link to="/venues">Venues</Link></span>
         </div>
       </Router>
     );
